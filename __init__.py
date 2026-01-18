54249d4f97f5b468c6703bb3268cb9a96983a095
Kevin Abraham Wilson <psychward41@gmail.com>
2026-01-18 23:35:40 +0530

    new one touching

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # kimura/__init__.py - Expose public API
-from .session.worker import PQCWorker as worker
-from .session.master import PQCMaster as master  
+from .session.worker import PQCWorker as Worker
+from .session.master import PQCMaster as Master  
 from .session.manager import SessionManager
 
 __version__ = "0.1.0"
-__all__ = ["worker", "master", "SessionManager"]
+__all__ = ["Worker", "Master", "SessionManager"]
